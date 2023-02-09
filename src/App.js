42ccf614490272fa0a43bdcb8858f0c894fc9c9c
MSiesto <miguelsiesto@gmail.com>
2023-02-09 13:05:03 +0100

    importing & implementing QueryClientProvider in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Cats />
+    </QueryClientProvider>
+  );
+}
+
+function Cats() {
+  return <div className="App">Cats</div>;
 }
 
 export default App;
