c5b5074069fcf0704c3f47cd824199c2e864c393
MSiesto <miguelsiesto@gmail.com>
2023-02-10 16:28:24 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { CharactersInfo } from "./functions";
-import { Card, CircularProgress } from "@mui/material";
+import {
+  Card,
+  CircularProgress,
+  Grid,
+  Container,
+  Typography,
+} from "@mui/material";
 
 import "./App.css";
 
@@ -15,16 +21,29 @@ function App() {
   }, []);
 
   return characters ? (
-    <div className="App">
-      {characters.data.map((character) => (
-        <Card {...character}>
-          <img src={character.image} alt="not found" />
-          <p>{character.name}</p>
-          <p>{character.gender}</p>
-          <p>Origin: {character.origin.name}</p>
-        </Card>
-      ))}
-    </div>
+    <Container className="App__Container">
+      <Grid className="grid" container columnGap={10} rowGap={5}>
+        {characters.data.map((character) => (
+          <Card
+            className="character_Card"
+            key={character.id}
+            {...character}
+            raised={true}
+          >
+            <img src={character.image} alt="not found" />
+            <Container className="textContainer" maxWidth="md">
+              <Typography variant="h5">{character.name}</Typography>
+              <br></br>
+              <Typography variant="overline">{character.gender}</Typography>
+              <br></br>
+              <Typography variant="subtitle1">
+                Origin: {character.origin.name}
+              </Typography>
+            </Container>
+          </Card>
+        ))}
+      </Grid>
+    </Container>
   ) : (
     <div className="App">
       <CircularProgress />
