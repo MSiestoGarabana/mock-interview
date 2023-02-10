4a44ceb54b0fece93f71506e83043a7803dabe3a
MSiesto <miguelsiesto@gmail.com>
2023-02-10 12:01:00 +0100

    Fixing state bug, painting data in CharacterCard

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { useEffect, useState } from "react";
 import { CharactersInfo } from "./functions";
 import "./App.css";
 
+function CharacterCard({ image, name, gender, origin }) {
+  return (
+    <div>
+      <img src={image} alt="not found" />
+      <p>Name: {name}</p>
+      <p>Gender: {gender}</p>
+      <p>Origin: {origin.name}</p>
+    </div>
+  );
+}
 function App() {
-  const [characters, setCharacters] = useState(null);
-  console.log(characters);
+  const [characters, setCharacters] = useState();
+
   useEffect(() => {
     CharactersInfo(setCharacters);
   }, []);
 
-  return <div>{characters.data[0].name}</div>;
+  console.log(characters);
+
+  //
+  return characters === undefined ? (
+    <div>loading</div>
+  ) : (
+    <div>
+      {characters.data.map((character) => (
+        <CharacterCard {...character} />
+      ))}
+    </div>
+  );
 }
 export default App;
